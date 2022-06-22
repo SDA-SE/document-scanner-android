80d49128ca837414ec1a8d0303dc852f098c8fd3
Frederic Hübner <frederic.huebner@sda.se>
2022-06-22 16:47:29 +0200

    chore: add and run spotless gradle plugin

--- documentscanner/src/test/java/com/websitebeaver/documentscanner/ExampleUnitTest.kt
+++ documentscanner/src/test/java/com/websitebeaver/documentscanner/ExampleUnitTest.kt
@@ -1,16 +1,13 @@
 package com.websitebeaver.documentscanner
 
 import com.websitebeaver.documentscanner.constants.ResponseType
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Unit test to make sure ResponseType constants are the right value
- */
+/** Unit test to make sure ResponseType constants are the right value */
 class ResponseTypeUnitTest {
-    @Test
-    fun constantValue_isCorrect() {
-        assertEquals(ResponseType.BASE64, "base64")
-    }
+  @Test
+  fun constantValue_isCorrect() {
+    assertEquals(ResponseType.BASE64, "base64")
+  }
 }
